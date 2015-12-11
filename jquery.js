72136466f66dedb4e21eb1e9333a3f09ef2897cb
eremmele <ericaremmele@Ericas-MacBook-Pro-2.local>
2015-12-11 18:43:39 -0500

    add

--- jquery.js
+++ jquery.js
@@ -23,20 +23,3 @@ $(document).ready(function(){
 
 //////////////////////////////
 
-
-/**
- * Check a href for an anchor. If exists, and in document, scroll to it.
- * If href argument ommited, assumes context (this) is HTML Element,
- * which will be the case when invoked by jQuery after an event
- */
-
-    var $content = $('#two');
-    $('.seework').each(function(){
-        var $this=$(this);
-            scrollTo = 0;
-        $this.on('click', function(){
-            var $target = $('#'+$this.data('scrollid'));
-            scrollTo = scrollTo + $target.position().top + $target.scrollTop();
-            $content.animate({scrollTop: scrollTo}, 1000);
-        })
-    })
