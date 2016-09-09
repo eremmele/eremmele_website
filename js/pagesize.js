2ed666970bcd7f0a51b15497788814d7e892dab6
eremmele <ericaremmele@Ericas-MacBook-Pro-2.local>
2016-09-08 21:57:22 -0400

    REVAMP pt 1

---
+++ js/pagesize.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('.sidebar').css('min-height', windowHeight);
+  };
+  setHeight();
+  
+  $(window).resize(function() {
+    setHeight();
+  });
+});
