79f5be2182f6d5c0eddfe78582fd5b46e90de362
eremmele <ericaremmele@Ericas-MacBook-Pro-2.local>
2016-11-20 20:15:51 -0500

    long overdue

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,9 @@
 //ABOUT effect
 
-$(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    var height = $(window).height();
-    $('.fade').css({
-        'opacity': ((height - scrollTop) / height)
-    });
-});
-
-//$(document).ready(function(){
-//    $(".btn1").click(function(){
-//        $("#rotator").slideUp();
-//    });
-//    $(".btn2").click(function(){
-//       $("#rotator").slideDown();
-//    });
-//});
-
-$(document).ready(function(){
-		$('#changeText').click(function(){
-			$('#content').text('#newcontent');
-		});
-	});
-
-//////////////////////////////
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
