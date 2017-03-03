5e1e692c3ba27a7e034bf6fea4386ea4b9b14b43
eremmele <ericaremmele@Ericas-MacBook-Pro-2.local>
2017-03-03 02:52:48 -0600

    new loader

--- js/load.js
+++
@@ -1,6 +0,0 @@
-//paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$j(window).load(function() {
-		// Animate loader off screen
-		$j(".se-pre-con").fadeOut("slow");;
-	});
---
+++ js/preloader.js
@@ -0,0 +1,15 @@
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$j(window).load(function() {
+		// Animate loader off screen
+		$j("body").addClass("loaded");;
+	});
+
+//$j(document).ready(function() {
+	
+//	setTimeout(function(){
+//		$j('body').addClass('loaded');
+		
+//	}, 100); //3000
+	
+//});
