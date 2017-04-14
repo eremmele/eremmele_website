884ece536ec17fa92aeb2609a9c19e368a25dad8
eremmele <ericaremmele@Ericas-MacBook-Pro-2.local>
2017-04-14 11:36:41 -0500

    small updates

--- js/jquery.js
+++
@@ -1,9 +0,0 @@
-//ABOUT effect
-
-function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block')
-          e.style.display = 'none';
-       else
-          e.style.display = 'block';
-    }
